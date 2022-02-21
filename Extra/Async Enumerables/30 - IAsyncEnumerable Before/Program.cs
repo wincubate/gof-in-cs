c4bcc092e0aacec010d7631484b4444464b0eb8d
Jesper Gulmann Henriksen <jesper@gulmann.dk>
2022-02-21 14:03:52 +0100

    Added extras

---
+++ Extra/Async Enumerables/30 - IAsyncEnumerable Before/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Wincubate.CS8.Slide30
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] urls = new string[]
+            { 
+                "http://www.jp.dk", 
+                "http://dr.dk", 
+                "http://bold.dk" 
+            };
+            foreach (string s in GetWords(urls))
+            {
+                Console.WriteLine(s);
+                Console.WriteLine();
+            }
+        }
+
+        static IEnumerable<string> GetWords(string[] urls)
+        {
+            foreach (var url in urls)
+            {
+                WebClient wc = new WebClient();
+                string result = wc.DownloadString(url);
+                yield return result.Substring(0, 256);
+            }
+        }
+    }
+}
---
+++ Extra/Async Enumerables/30 - IAsyncEnumerable Complete/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Wincubate.Threading.Module10
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string[] urls = new string[]
+            { 
+                "http://www.jp.dk", 
+                "http://dr.dk", 
+                "http://bold.dk" 
+            };
+            await foreach (string s in GetWordsAsync(urls))
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static async IAsyncEnumerable<string> GetWordsAsync(string[] urls)
+        {
+            foreach (var url in urls)
+            {
+                WebClient wc = new WebClient();
+                string result = await wc.DownloadStringTaskAsync(url);
+                yield return result.Substring(0, 256);
+            }
+        }
+    }
+}
